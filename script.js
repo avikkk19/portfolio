dadd0e32d076f0836b2334bb52bcbd94d3b8efe2
avikkk19 <avinashkamadri14@gmail.com>
2024-06-08 19:26:15 +0530

    added response

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-    function scrollToElement(elementSelector , instance = 0){
-        const elements = document.querySelectorAll(elementSelector);
-        if(elements.length > instance){
-            elements[instance].scrollIntoView({behavior : 'smooth'});
-        }
-    };
+function scrollToElement(elementSelector, instance = 0){
+    const elements = document.querySelectorAll(elementSelector);
+    if(elements.length > instance){
+        elements[instance].scrollIntoView({behavior : `smooth`})
+    }
+}
 
 
-const pepe = document.getElementById("pepe");
-pepe.addEventListener('click' , () =>{
-    scrollToElement('.contact')
+ const docsid = document.getElementById("docsid");
+ 
+ docsid.addEventListener(`click`, () =>{
+    scrollToElement(".contact");
+    });
+const projectsid = document.getElementById("projectsid");
+
+projectsid.addEventListener("click", () =>{
+    scrollToElement(".projects");
 });
-const projectbtn = document.getElementById("projectbtn");
-projectbtn.addEventListener('click', () =>{
-    scrollToElement('.projects')
+
+const featuredid = document.getElementById("featuredid");
+featuredid.addEventListener("click", () =>{
+    scrollToElement(".footer")
+})
+
+const aboutid = document.getElementById("aboutid");
+aboutid.addEventListener("click", () =>{
+    scrollToElement(".footer")
+})
+
+const totop = document.getElementById("totop");
+totop.addEventListener('click', () =>{
+    scrollToElement(".hero")
 })
